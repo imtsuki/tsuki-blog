4961fdfab9c2647cacc22f1dd3e3dc1651994bfe
imtsuki <me@qjx.app>
2020-06-26 22:50:09 +0800

    update katex cdn url
    Signed-off-by: imtsuki <me@qjx.app>

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -57,12 +57,7 @@ const IndexLayout: React.FC = ({ children }) => (
         >
           {/* eslint-disable-next-line jsx-a11y/lang */}
           <html lang="zh-Hans" />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min.css"
-            integrity="sha256-V8SV2MO1FUb63Bwht5Wx9x6PVHNa02gv8BgH/uH3ung="
-            crossOrigin="anonymous"
-          />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" />
         </Helmet>
         <Header title={data.site.siteMetadata.title} />
         <LayoutMain>{children}</LayoutMain>
