17226dba412f4455b953b169d19eb7c0b457fac4
imtsuki <me@qjx.app>
2020-08-10 19:40:52 +0800

    add rss feed
    Signed-off-by: imtsuki <me@qjx.app>

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,59 @@ module.exports = {
         showSpinner: false
       }
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                }
+              }
+            }
+          `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges
+                .filter((edge) => edge.node.fields.slug.startsWith('/posts'))
+                .map((edge) => {
+                  return {
+                    ...edge.node.frontmatter,
+                    description: edge.node.excerpt,
+                    data: edge.node.frontmatter.date,
+                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    custom_elements: [{ 'content:encoded': edge.node.html }]
+                  };
+                });
+            },
+            query: `
+              {
+                allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
+                  edges {
+                    node {
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                      html
+                    }
+                  }
+                }
+              }
+              `,
+            output: '/rss.xml',
+            title: `ツキの月 | RSS Feed`,
+            site_url: `https://qjx.app`
+          }
+        ]
+      }
+    },
     'gatsby-transformer-json',
     {
       resolve: 'gatsby-plugin-canonical-urls',
