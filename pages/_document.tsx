a70842b9e3666d3c0a0842a7ca516e8532005285
imtsuki <me@qjx.app>
2022-10-31 00:45:48 -0400

    add google site verification
    Signed-off-by: imtsuki <me@qjx.app>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,11 @@ const Document = () => {
   return (
     <Html className="text-black dark:bg-zinc-900 dark:text-white">
       <Head>
+        <meta
+          name="google-site-verification"
+          content="Sf3TQAKc2EfeBR3ltsbYlbX06bHJ4-QbsC_7Jko3zZ8"
+        />
+
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.css"
