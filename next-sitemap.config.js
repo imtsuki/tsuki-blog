0867ef2087983fe2023de3032129f01371d45e13
imtsuki <me@qjx.app>
2022-11-01 00:31:07 -0400

    generate sitemap
    Signed-off-by: imtsuki <me@qjx.app>

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const nextSitemapConfig = {
+  siteUrl: 'https://qjx.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
+
+export default nextSitemapConfig;
