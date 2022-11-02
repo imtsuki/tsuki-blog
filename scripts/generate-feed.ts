97f4893b8a942a8ad37a2454064af254b3c85e4c
imtsuki <me@qjx.app>
2022-11-02 12:35:43 -0400

    generate rss feed
    Signed-off-by: imtsuki <me@qjx.app>

--- scripts/generate-feed.ts
+++ scripts/generate-feed.ts
@@ -1,2 +1,50 @@
+import fs from 'fs';
 import { Feed } from 'feed';
+import matter from 'gray-matter';
+import { postSlugs, getSourceBySlug } from '../lib/content.js';
 import siteConfig from '../site.config.js';
+
+const getPublicUrlBySlug = (slug: string) => `${siteConfig.url}/posts/${slug}`;
+
+console.log('Generating RSS feed...');
+
+const feed = new Feed({
+  title: siteConfig.title,
+  description: siteConfig.description,
+  id: siteConfig.url,
+  link: siteConfig.url,
+  image: `${siteConfig.url}/icon.png`,
+  favicon: `${siteConfig.url}/favicon.ico`,
+  copyright: siteConfig.author,
+  author: {
+    name: siteConfig.author,
+    link: siteConfig.url,
+  },
+});
+
+const items = await Promise.all(
+  postSlugs.map(async (slug) => {
+    const source = await getSourceBySlug(slug);
+    const { data } = matter(source);
+    return {
+      title: data.title,
+      id: getPublicUrlBySlug(slug),
+      link: getPublicUrlBySlug(slug),
+      date: data.date,
+    };
+  })
+);
+
+console.log(`Found ${items.length} posts`);
+
+items.sort((a, b) => (a.date > b.date ? -1 : 1));
+
+items.forEach((item) => feed.addItem(item));
+
+console.log('Writing RSS feed to public/feed.xml');
+
+const rss = feed.rss2();
+
+await fs.promises.writeFile('public/feed.xml', rss);
+
+console.log('Done generating RSS feed');
