2c63ab64254fc127719a4add848bb26414130afa
imtsuki <me@qjx.app>
2022-11-04 16:47:52 -0400

    add meta description
    Signed-off-by: imtsuki <me@qjx.app>

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,4 +1,4 @@
-import NextHead from 'next/head';
+import Head from 'next/head';
 import { PropsWithChildren } from 'react';
 import siteConfig from '../site.config.js';
 
@@ -10,38 +10,33 @@ interface HeadProps extends PropsWithChildren {
 export const Meta = ({ title, description, children }: HeadProps) => {
   const { twitter } = siteConfig.author;
   const pageTitle = title ? `${title} | ${siteConfig.title}` : siteConfig.title;
+  const pageDescription = description ?? pageTitle;
   return (
-    <NextHead>
+    <Head>
       <title>{pageTitle}</title>
-      {description && (
-        <meta name="description" content={description} key="description" />
-      )}
+      <meta name="description" content={pageDescription} key="description" />
 
       {/* Open Graph */}
       <meta property="og:title" content={pageTitle} key="og:title" />
-      {description && (
-        <meta
-          property="og:description"
-          content={description}
-          key="og:description"
-        />
-      )}
+      <meta
+        property="og:description"
+        content={pageDescription}
+        key="og:description"
+      />
 
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary" key="twitter:card" />
       <meta name="twitter:site" content={twitter} key="twitter:site" />
       <meta name="twitter:creator" content={twitter} key="twitter:creator" />
       <meta name="twitter:title" content={pageTitle} key="twitter:title" />
-      {description && (
-        <meta
-          name="twitter:description"
-          content={description}
-          key="twitter:description"
-        />
-      )}
+      <meta
+        name="twitter:description"
+        content={pageDescription}
+        key="twitter:description"
+      />
 
       {/* Other children */}
       {children}
-    </NextHead>
+    </Head>
   );
 };
