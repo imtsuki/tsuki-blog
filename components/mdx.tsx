2481b9a3e79d79518486a1e1467747b8ba0a3d26
imtsuki <me@qjx.app>
2023-02-01 01:20:30 -0500

    implement our own rough-notation wrapper
    support scroll animation by using `IntersectionObserver`
    
    Signed-off-by: imtsuki <me@qjx.app>

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,21 +1,7 @@
-import React from 'react';
 import Image, { type ImageProps } from 'next/image';
 
 import { Callout } from 'components/callout';
-
-// TODO: fix react rough notation server component error
-const Underline = ({ children }: { children: React.ReactNode }) => (
-  <span>{children}</span>
-);
-const Box = ({ children }: { children: React.ReactNode }) => (
-  <span>{children}</span>
-);
-const Circle = ({ children }: { children: React.ReactNode }) => (
-  <span>{children}</span>
-);
-const Highlight = ({ children }: { children: React.ReactNode }) => (
-  <span>{children}</span>
-);
+import { Annotation } from 'components/annotation';
 
 /** Custom components/renderers to pass to MDX. */
 export const mdxComponents = {
@@ -40,13 +26,6 @@ export const mdxComponents = {
       {title && <figcaption>{title}</figcaption>}
     </figure>
   ),
+  Annotation,
   Callout,
-  // Underline: dynamic(() => import('./Annotation').then((mod) => mod.Underline)),
-  // Box: dynamic(() => import('./Annotation').then((mod) => mod.Box)),
-  // Circle: dynamic(() => import('./Annotation').then((mod) => mod.Circle)),
-  // Highlight: dynamic(() => import('./Annotation').then((mod) => mod.Highlight)),
-  Underline,
-  Box,
-  Circle,
-  Highlight,
 };
---
+++ lib/directives.tsx
@@ -0,0 +1,61 @@
+import { visit } from 'unist-util-visit';
+
+import { AnnotationProps } from 'components/annotation';
+import { CalloutProps } from 'components/callout';
+
+export const enforceArrayMembers =
+  <T extends unknown>() =>
+  <U extends T[]>(
+    array: U & ([T] extends [U[number]] ? unknown : 'missing array member(s)')
+  ) =>
+    array;
+
+export const CALLOUT_TYPES = enforceArrayMembers<
+  NonNullable<CalloutProps['type']>
+>()(['note', 'info', 'tip', 'warning', 'danger']);
+
+export const ANNOTATION_TYPES = enforceArrayMembers<
+  NonNullable<AnnotationProps['type']>
+>()(['box', 'circle', 'highlight', 'underline']);
+
+const visitAnnotation = (node: any) => {
+  if (node.type !== 'textDirective') return;
+
+  const annotationType = node.name;
+
+  node.type = 'mdxJsxTextElement';
+  node.name = 'Annotation';
+  node.attributes = [
+    { type: 'mdxJsxAttribute', name: 'type', value: annotationType },
+  ];
+};
+
+const visitCallout = (node: any) => {
+  if (node.type !== 'containerDirective') return;
+
+  const calloutType = node.name;
+
+  node.type = 'mdxJsxFlowElement';
+  node.name = 'Callout';
+  node.attributes = [
+    { type: 'mdxJsxAttribute', name: 'type', value: calloutType },
+  ];
+};
+
+export const remarkTransformDirectives = () => {
+  // annotating types here causes TypeScript language server to lag
+  const transformer = (tree: any) => {
+    visit(tree, (node) => {
+      if (ANNOTATION_TYPES.includes(node.name)) {
+        visitAnnotation(node);
+        return;
+      }
+      if (CALLOUT_TYPES.includes(node.name)) {
+        visitCallout(node);
+        return;
+      }
+    });
+  };
+
+  return transformer;
+};
